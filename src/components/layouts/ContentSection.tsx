a81a87c46713e00c4d18d88b0ac2d28289fcce56
Hameed Jimoh <hameed.jimoh1601@gmail.com>
2021-07-04 18:04:58 +0100

    Integrated error message in the UI

--- src/components/layouts/ContentSection.tsx
+++ src/components/layouts/ContentSection.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from "react";
 import { useTypedSelector } from "../hooks/useTypedSelector";
 import { useActions } from "./../hooks/useActions";
 import RectangularCard from "../RectangularCard";
@@ -5,7 +6,7 @@ import SquareCard from "../SquareCard";
 import Footer from "./Footer";
 
 const ContentSection: React.FC = () => {
-  const { weatherData } = useTypedSelector((state) => state.weather);
+  const { weatherData, error } = useTypedSelector((state) => state.weather);
   const { getDegreeCelsius, getDegreeFahrenheit } = useActions();
   return (
     <section className="section">
@@ -23,16 +24,25 @@ const ContentSection: React.FC = () => {
           <sup>o</sup>F
         </button>
       </div>
-      <div className="card">
-        {weatherData &&
-          weatherData.consolidated_weather
-            .slice(1)
-            .map((data: any) => <SquareCard key={data.id} day={data} />)}
-      </div>
-      <div className="section__highlights">
-        <h2 className="section__highlights__title">Today's highlights</h2>
-        <RectangularCard />
-      </div>
+      {error ? (
+        <div className="section__error">
+          <h2 className="section__error__text">{error}</h2>
+        </div>
+      ) : (
+        <Fragment>
+          <div className="card">
+            {weatherData &&
+              weatherData.consolidated_weather
+                .slice(1)
+                .map((data: any) => <SquareCard key={data.id} day={data} />)}
+          </div>
+          <div className="section__highlights">
+            <h2 className="section__highlights__title">Today's highlights</h2>
+            <RectangularCard />
+          </div>
+        </Fragment>
+      )}
+
       <Footer />
     </section>
   );
